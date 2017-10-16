9a047667b493a41d928ea3cf079d5f2eb117d295
WinstonH <bjdhjj@gmail.com>
2017-10-17 00:13:41 +0800

    Create entrypoint.sh

---
+++ ssr/http/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# launch website
+nginx
+# launch ssr
+python /shadowsocksr/shadowsocks/server.py -p $PORT -k $PASSWD -m $METHOD -O $PROTOCOL -o $OBFS -d start
+# config kcptun
+sed -i "s/listen_port/${listen_port}/g" /kcptun/${MODE}-config.json
+sed -i "s/remote_server:remote_port/${remote_server}:${remote_port}/g" /kcptun/${MODE}-config.json
+# launch kcptun server or client
+/kcptun/${MODE}_linux_amd64 -c /kcptun/${MODE}-config.json
