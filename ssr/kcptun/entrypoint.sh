91f72e195f1f9117678715354bb5fa006ad8770c
WinstonH <bjdhjj@gmail.com>
2017-10-16 19:04:34 +0800

    Update entrypoint.sh

--- ssr/kcptun/entrypoint.sh
+++ ssr/kcptun/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # launch ssr
 python /shadowsocksr/shadowsocks/server.py -p $PORT -k $PASSWD -m $METHOD -O $PROTOCOL -o $OBFS -d start
+# config kcptun
+sed -i 's/listen_port/${listen_port}/g' /kcptun/${MODE}-config.json
+sed -i 's/remote_server:remote_port/${remote_server}:{remote_port}/g' /kcptun/${MODE}-config.json
 # launch kcptun server or client
 /kcptun/${MODE}_linux_amd64 -c /kcptun/${MODE}-config.json 2>&1 &
