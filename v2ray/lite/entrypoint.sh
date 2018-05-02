3ca726cf916d437b9804c2b54b2be8c4eb236209
Winston <bjdhjj@gmail.com>
2018-05-02 16:28:59 +0800

    Update entrypoint.sh

--- v2ray/lite/entrypoint.sh
+++ v2ray/lite/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 if [ -n "$UUID" ];then
     sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You've set an UUID"
+    echo "The UUID is: $UUID"
 else
-    export UUID=$(uuidgen) && sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json && echo "New generated UUID:$UUID"
+    export UUID=$(uuidgen)
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You didn't set an UUID"
+    echo "The auto-generated UUID is: $UUID"
 fi
 
 sed -i "s/PORT/$PORT/g" /etc/v2ray/config.json
