ed03fa23adbefd9d44f1909fdcb0c3e6b4a18c32
Winston <bjdhjj@gmail.com>
2018-05-02 17:07:37 +0800

    update

---
+++ v2ray/websocket/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ -n "$UUID" ];then
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You've set an UUID"
+    echo "The UUID is: $UUID"
+else
+    export UUID=$(uuidgen)
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+    echo "You didn't set an UUID"
+    echo "The auto-generated UUID is: $UUID"
+fi
+
+sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
+v2ray -config=/etc/v2ray/config.json
