436dc58927032e3a6708de91cdde667cbef27bc4
Winston <bjdhjj@gmail.com>
2018-05-02 16:24:38 +0800

    auto-generate UUID now

--- v2ray/lite/entrypoint.sh
+++ v2ray/lite/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+if [ -n "$UUID" ];then
+    sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
+else
+    export UUID=$(uuidgen) && sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json && echo "New generated UUID:$UUID"
+fi
+
 sed -i "s/PORT/$PORT/g" /etc/v2ray/config.json
 v2ray -config=/etc/v2ray/config.json
-
